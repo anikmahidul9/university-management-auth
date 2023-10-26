ffddb09e57df294107d0e95db95a7fb803d2a766
anikmahidul9 <mahidulanik9@gmail.com>
2023-10-27 00:42:21 +0600

    mongose-env-database

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import config from './config/index'
+import app from './app'
+
+async function main() {
+    try{
+        await mongoose.connect(config.dataBase_url as string);
+
+        app.listen(config.port, () => {
+          console.log(`Example app listening on port ${config.port}`);
+        })
+    }catch (error) {
+        console.log(error)
+      }
+}
+
+main();
