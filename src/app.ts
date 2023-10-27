c70e5c06713279398ca8ea06d2d6b36209cbfd28
anikmahidul9 <mahidulanik9@gmail.com>
2023-10-28 00:21:43 +0600

    add-husky-stagelint

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,8 @@ import cors from 'cors'
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({ extended: any }))
-app.get('/', (req: Request, res: Response) => {
+app.use(express.urlencoded({ extended: true }))
+app.get('/', (req: Request, res: any) => {
   res.send('Hello World!')
 })
 
